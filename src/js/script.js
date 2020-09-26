fd93041b3bce5dc2d9c469e1af7bcf6f2ba12958
Miszczo <69727417+Miszczo@users.noreply.github.com>
2020-09-27 01:13:59 +0200

    start of initAccorion function

--- src/js/script.js
+++ src/js/script.js
@@ -55,10 +55,55 @@
   class Product{
     constructor(id, data){
       const thisProduct = this;
+
       thisProduct.id = id;
       thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
       console.log('new Product:', thisProduct);
     }
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      // console.log(generatedHTML);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+    const thisProduct = this;
+
+    /* find the clickable trigger (the element that should react to clicking) */
+
+    /* START: click event listener to trigger */
+
+      /* prevent default action for event */
+
+      /* toggle active class on element of thisProduct */
+
+      /* find all active products */
+
+      /* START LOOP: for each active product */
+
+        /* START: if the active product isn't the element of thisProduct */
+
+          /* remove class active for the active product */
+
+        /* END: if the active product isn't the element of thisProduct */
+
+      /* END LOOP: for each active product */
+
+    /* END: click event listener to trigger */
+      }
+    }
+
   }
   console.log(Product);
 
