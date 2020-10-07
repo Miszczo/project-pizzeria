222bc63cc6814eb4d6abd4b4e28be1ec0accb8b5
Miszczo <69727417+Miszczo@users.noreply.github.com>
2020-10-07 18:58:36 +0200

    Add cart changes and repair cart counter- repair mistake in deliveryFee camel case notation

--- src/js/script.js
+++ src/js/script.js
@@ -355,7 +355,9 @@
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
 
@@ -366,7 +368,7 @@
 
       thisCart.products = [];
 
-      thisCart.deliveryfee = settings.cart.defaultDeliveryFee;
+      thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
       thisCart.getElements(element);
       thisCart.initActions();
       thisCart.update();
@@ -381,6 +383,13 @@
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
       thisCart.dom.productList = document.querySelector(select.cart.productList);
+
+      thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
+
+      for(let key of thisCart.renderTotalsKeys){
+        thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
+      }
+
     }
 
     initActions(){
@@ -394,6 +403,10 @@
       const generatedHTML = templates.cartProduct(menuProduct);
 
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
+      });
 
       thisCart.dom.productList.appendChild(generatedDOM);
       // console.log('adding product', menuProduct);
@@ -417,6 +430,14 @@
       thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
       console.log(thisCart.totalNumber);
       console.log(thisCart.subtotalPrice);
+      console.log(thisCart.totalPrice);
+      console.log(thisCart.deliveryFee);
+
+      for(let key of thisCart.renderTotalsKeys){
+        for(let elem of thisCart.dom[key]){
+          elem.innerHTML = thisCart[key];
+        }
+      }
     }
   }
   class CartProduct {
