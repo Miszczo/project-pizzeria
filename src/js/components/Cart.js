638e8177c0d5f397162be1e18addcb09af15ddb1
Miszczo <69727417+Miszczo@users.noreply.github.com>
2020-10-10 17:09:19 +0200

    separate js files

---
+++ src/js/components/Cart.js
@@ -0,0 +1,141 @@
+export class Cart {
+  constructor(element){
+    const thisCart = this;
+
+    thisCart.products = [];
+
+    thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
+    thisCart.getElements(element);
+    thisCart.initActions(element);
+
+    console.log('new Cart', thisCart);
+    console.log(Cart);
+  }
+
+  getElements(element){
+    const thisCart = this;
+
+    thisCart.dom = {};
+    thisCart.dom.wrapper = element;
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    thisCart.dom.productList = document.querySelector(select.cart.productList);
+    thisCart.dom.form = document.querySelector(select.cart.form);
+
+    thisCart.dom.inputPhone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+    thisCart.dom.inputAddress = thisCart.dom.wrapper.querySelector(select.cart.address);
+
+    thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
+
+    for(let key of thisCart.renderTotalsKeys){
+      thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
+    }
+
+  }
+
+  initActions(){
+    const thisCart = this;
+
+    thisCart.dom.toggleTrigger.addEventListener('click', function() {
+      thisCart.dom.wrapper.classList.toggle('active');
+    });
+
+    thisCart.dom.productList.addEventListener('updated', function() {
+      thisCart.update();
+    });
+
+    thisCart.dom.productList.addEventListener('remove', function(event) {
+      thisCart.remove(event.detail.cartProduct);
+    });
+    thisCart.dom.form.addEventListener('submit', function(event){
+      event.preventDefault();
+      thisCart.sendOrder();
+    });
+  }
+  add(menuProduct){
+    const thisCart = this;
+    const generatedHTML = templates.cartProduct(menuProduct);
+
+    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+
+    thisCart.dom.productList.addEventListener('updated', function(){
+      thisCart.update();
+    });
+
+    thisCart.dom.productList.appendChild(generatedDOM);
+    // console.log('adding product', menuProduct);
+
+    thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+    // console.log('thisCart.products', thisCart.products);
+    thisCart.update();
+  }
+  update(){
+    const thisCart = this;
+
+    thisCart.totalNumber = 0;
+    thisCart.subtotalPrice = 0;
+
+    for (let thisCartProduct of thisCart.products) {
+
+      thisCart.subtotalPrice += thisCartProduct.price;
+      thisCart.totalNumber += thisCartProduct.amount;
+    }
+
+    thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
+    console.log(thisCart.totalNumber);
+    console.log(thisCart.subtotalPrice);
+    console.log(thisCart.totalPrice);
+    console.log(thisCart.deliveryFee);
+
+    for(let key of thisCart.renderTotalsKeys){
+      for(let elem of thisCart.dom[key]){
+        elem.innerHTML = thisCart[key];
+      }
+    }
+  }
+  remove(cartProduct) {
+    const thisCart = this;
+
+    const index = thisCart.products.indexOf(cartProduct);
+    console.log(index);
+
+    thisCart.products.splice(index, 1);
+
+    cartProduct.dom.wrapper.remove();
+
+    thisCart.update();
+  }
+  sendOrder(){
+    const thisCart = this;
+    const url = settings.db.url + '/' + settings.db.order;
+
+    const payload = {
+      products: [],
+      totalNumber: thisCart.totalNumber,
+      deliveryFee: thisCart.deliveryFee,
+      subtotalPrice: thisCart.subtotalPrice,
+      totalPrice: thisCart.totalPrice,
+      phone: thisCart.dom.inputPhone.value,
+      address: thisCart.dom.inputAddress.value,
+    };
+    for(let thisCartProduct of thisCart.products){
+      const productData = thisCartProduct.getData();
+
+      payload.products.push(productData);
+    }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, options)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+}
